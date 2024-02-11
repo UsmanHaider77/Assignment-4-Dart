755027e6297d0d6baaa0d593933fa15e7513631c
UsmanHaider77 <musmanhaider77@gmail.com>
2024-02-10 22:12:57 -0800

    Right Angle Triangle With Asterisk

---
+++ asterisk_triangle.dart
@@ -0,0 +1,8 @@
+
+ void main() {
+  String star = "*";
+  for (int i = 1; i <= 4; i++) {
+    String number = "$star" * i ;
+    print(number);
+  }
+} 
---
+++ triangle_asterisk.dart
@@ -0,0 +1,9 @@
+
+
+  void main() {
+  String star = "*";
+  for (int i = 1; i <= 4; i++) {
+    String number = "$star" * i ;
+    print(number);
+  }
+}              
